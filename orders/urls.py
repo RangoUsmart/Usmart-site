5023a238294f88697db1cc507adcc8f6230c9fc3
rango <dev.rango@git.ua>
2023-05-19 17:01:49 +0300

    FROM PC

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-# from .views import PayView, PayCallbackView
-from . import views
+from .views import PayView, PayCallbackView
+# from .views import  PayCallbackView
+# from . import views
 
 app_name = 'orders' 
 
 urlpatterns = [
-    path('', views.order_create, name='order_create'),
-    # path('', PayView.as_view(), name='order_create'),
-    # path('pay-callback/', PayCallbackView.as_view(), name='pay_callback')
+    # path('', views.order_create, name='order_create'),
+    path('', PayView.as_view(), name='order_create'),
+    path('', PayCallbackView.as_view(), name='pay_callback')
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -14,85 +14,88 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 
+import random
+import requests
 
 public_key=os.getenv('PAYMENTS_PUBLIC_KEY')
 privat_key=os.getenv('PAYMENTS_SECRET_KEY')
 
-# class PayView(TemplateView):
-#     template_name = 'create.html'
+class PayView(TemplateView):
+    template_name = 'create.html'
 
-#     def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         
-#         liqpay = LiqPay(public_key, privat_key)
-#         cart = Cart(request)
-#         amount=str(cart.get_total_price())
-#         # OrderItem.objects.create(        order=order,
-#         #                                  product=item['product'],
-#         #                                  price=item['price'],
-#         #                                  quantity=item['quantity'])
-#         # очистка корзины
-#         # cart.clear()
-#         params = {
-#             'action': 'pay',
-#             'amount': amount,
-#             'currency': 'UAH',
-#             'description': 'Payment for clothes',
-#             'order_id': 'order_id_3',
-#             'version': '3',
-#             'sandbox': 1, # sandbox mode, set to 1 to enable it
-#             # 'server_url': 'http://localhost/create/pay-callback/', # url to callback view
-#             'result_url': 'http://localhost/create/pay-callback/',
-#         }
-#         signature = liqpay.cnb_signature(params)
-#         data = liqpay.cnb_data(params)
-#         return render(request, self.template_name, {'signature': signature, 'data': data})
+        liqpay = LiqPay(public_key, privat_key)
+        cart = Cart(request)
+        amount=str(cart.get_total_price())
+        # OrderItem.objects.create(        order=order,
+        #                                  product=item['product'],
+        #                                  price=item['price'],
+        #                                  quantity=item['quantity'])
+        # очистка корзины
+        # cart.clear()
+        params = {
+            'action': 'pay',
+            'amount': amount,
+            'currency': 'UAH',
+            'description': 'Payment for clothes',
+            'order_id': random.randint(1, 1000),
+            'version': '3',
+            'sandbox': 1, # sandbox mode, set to 1 to enable it
+            # 'server_url': 'http://localhost/create/pay-callback/', # url to callback view
+            'result_url': 'http://localhost/create/pay-callback/',
+        }
+        signature = liqpay.cnb_signature(params)
+        data = liqpay.cnb_data(params)
+        # print(request.POST.items)
+        return render(request, self.template_name, {'signature': signature, 'data': data})
 
-# @method_decorator(csrf_exempt, name='dispatch')
-# class PayCallbackView(View):
-#     def post(self, request, *args, **kwargs):
-#         liqpay = LiqPay(public_key, privat_key)
-#         data = request.POST.get('data')
-#         signature = request.POST.get('signature')
-#         sign = liqpay.str_to_sign(public_key + data + privat_key)
-#         if sign == signature:
-#             print('callback is valid')
-#         response = liqpay.decode_data_from_str(data)
-#         print('121212')
-#         print('callback data', response)
-#         return HttpResponse()
+@method_decorator(csrf_exempt, name='dispatch')
+class PayCallbackView(View):
+    def post(self, request, *args, **kwargs):
+        liqpay = LiqPay(public_key, privat_key)
+        data = request.POST.get('data')
+        signature = request.POST.get('signature')
+        sign = liqpay.str_to_sign(public_key + data + privat_key)
+        if sign == signature:
+            print('callback is valid')
+        response = liqpay.decode_data_from_str(data)
+        print('121212')
+        print('callback data', response)
+        return HttpResponse()
 
-def order_create(request):
-    cart = Cart(request)
-    # public_key=os.getenv('PAYMENTS_PUBLIC_KEY')
-    # privat_key=os.getenv('PAYMENTS_SECRET_KEY')
-    print(privat_key)
-    cart = Cart(request)
-    amount=str(cart.get_total_price())
-    liqpay = LiqPay(public_key, privat_key)
-    html = liqpay.cnb_form({
-        'action': 'pay',
-        'amount': amount,
-        'currency': 'UAH',
-        'description': 'description text',
-        'order_id': '1',
-        'version': '3',
-        'result_url': 'http://localhost/create/pay-callback/',
-    })
+# def order_create(request):
+#     cart = Cart(request)
+#     # public_key=os.getenv('PAYMENTS_PUBLIC_KEY')
+#     # privat_key=os.getenv('PAYMENTS_SECRET_KEY')
+#     print(privat_key)
+#     cart = Cart(request)
+#     amount=str(cart.get_total_price())
+#     liqpay = LiqPay(public_key, privat_key)
+#     html = liqpay.cnb_form({
+#         'action': 'pay',
+#         'amount': amount,
+#         'currency': 'UAH',
+#         'description': 'description text',
+#         'order_id': random.randint(1, 100),
+#         'version': '3',
+#         'result_url': 'http://localhost:8000/create/pay-callback/',
+#     })
     
-    if request.method == 'POST':
-        form = OrderCreateForm(request.POST)
-        if form.is_valid():
-            order = form.save()
-            for item in cart:
-                OrderItem.objects.create(order=order,
-                                         product=item['product'],
-                                         price=item['price'],
-                                         quantity=item['quantity'])
-            # очистка корзины
-            cart.clear()
-            return render(request, 'created.html',
-                          {'order': order})
-    else:
-        form = OrderCreateForm
-    return render(request, 'create.html',
-                  {'cart': cart, 'form': form, 'html':html})
+#     if request.method == 'POST':
+#         form = OrderCreateForm(request.POST)
+#         if form.is_valid():
+#             order = form.save()
+#             for item in cart:
+#                 OrderItem.objects.create(order=order,
+#                                          product=item['product'],
+#                                          price=item['price'],
+#                                          quantity=item['quantity'])
+#             # очистка корзины
+#             cart.clear()
+#             return render(request, 'created.html',
+#                           {'order': order})
+#     else:
+#         form = OrderCreateForm
+#     return render(request, 'create.html',
+#                   {'cart': cart, 'form': form, 'html':html})
